220d8349407325d9f9949933092951ee568e9049
molecule03 <laveshsalaskar619@gmail.com>
2026-03-14 19:22:05 +0530

    - Trie code udpate

--- src/DSA/DataStructures/NonLinear/Tries/Tries.java
+++ src/DSA/DataStructures/NonLinear/Tries/Tries.java
@@ -1,7 +1,9 @@
 package DSA.DataStructures.NonLinear.Tries;
 
 public class Tries {
-    public static int ALPHABET_SIZE=26;
+    private final int ALPHABET_SIZE=26;
+
+    // Node contains current value and child array which contains all the next alphabets
     private class Node{
         char value;
         Node[] children = new Node[ALPHABET_SIZE];
@@ -12,20 +14,49 @@ public class Tries {
         }
         @Override
         public String toString(){
-            return "value"+value;
+            return "value: "+value;
         }
     }
 
     private Node root = new Node(' ');
 
-    public void insert(String word){
+    public void insertWord(String word){
         Node current = root;
         for(char ch : word.toCharArray()){
             int index = ch -'a';
-            if(current.children[index]==null)
-            {current.children[index]=new Node(ch);}
+            if(current.children[index]==null) {
+                current.children[index]=new Node(ch);
+            }
             current = current.children[index];
         }
         current.isEndOfWord = true;
+    }
+
+    public boolean containsWord(String word){
+        Node current = root;
+        for(char ch : word.toCharArray()){
+            int index = ch-'a';
+            if(current.children[index] != null){
+                current = current.children[index];
+            }
+            else{
+                return false;
+            }
+        }
+        return current.isEndOfWord;
+    }
+
+    public boolean startsWith(String word){
+        Node current = root;
+        for(char ch : word.toCharArray()){
+            int index = ch-'a';
+            if(current.children[index] != null){
+                current = current.children[index];
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
     }
 }
